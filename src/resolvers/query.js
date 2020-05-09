c4574a1f6f699edeced1266f7f5c9c68c487b301
Merculies <merculies@outlook.com>
2020-05-10 01:06:11 +0700

    V T0102D05102020

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,39 +1,57 @@
-import bcrypt from "bcryptjs"
-import jwt from "jsonwebtoken"
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
-import User from "../models/user"
-import Product from "../models/product"
+import User from "../models/user";
+import Product from "../models/product";
 
 const Query = {
+  postsOffset: async (parent, { limit, offset }) => {
+    const cursor = Product.find();
+    if (limit) cursor.limit(limit);
+    if (offset) cursor.skip(offset);
+    return cursor;
+  },
   user: (parent, args, { userId }, info) => {
     // Check if user logged in
-    if (!userId) throw new Error("Please log in")
+    if (!userId) throw new Error("Please log in");
 
     return User.findById(userId)
       .populate({
         path: "products",
-        options: {sort: {createdAt: 'desc'}},
-        populate: { path: "user" }
+        options: { sort: { createdAt: "desc" } },
+        populate: { path: "user" },
       })
-      .populate({ path: "carts", populate: { path: "product" } })
+      .populate({ path: "carts", populate: { path: "product" } });
   },
   users: (parent, args, context, info) =>
     User.find({})
       .populate({
         path: "products",
-        populate: { path: "user" }
+        populate: { path: "user" },
       })
       .populate({ path: "carts", populate: { path: "product" } }),
   product: (parent, args, context, info) =>
     Product.findById(args.id).populate({
       path: "user",
-      populate: { path: "products" }
+      populate: { path: "products" },
     }),
-  products: (parent, args, context, info) =>
-    Product.find().populate({
+  productSearch: async (parent, args, context, info) => {
+    const cursor = Product.find({
+      description: { $regex: args.description }
+    }).populate({
       path: "user",
-      populate: { path: "products" }
-    }).sort({createdAt: 'desc'})
-}
+      populate: { path: "products" },
+    });
+    if (args.price) cursor.find({price:args.price})
+    return cursor;
+  },
+  products: (parent, args, context, info) =>
+    Product.find()
+      .populate({
+        path: "user",
+        populate: { path: "products" },
+      })
+      .sort({ createdAt: "desc" }),
+};
 
-export default Query
+export default Query;
